635e760369ee609225bd998c5263568690a3d4b2
teawon <xodnjsvkvk9@gmail.com>
2025-02-17 23:49:06 +0900

    init: 1주차 실습 pull

---
+++ app/esbuild.config.js
@@ -0,0 +1,17 @@
+import path from "node:path";
+import { build } from "esbuild";
+import { glob } from "glob";
+
+const entryPoints = await glob([path.resolve("./src/**/*.ts")]);
+
+await build({
+  entryPoints,
+  outdir: "dist",
+  target: "node20",
+  platform: "node",
+  bundle: false,
+  minify: false,
+  sourcemap: false,
+  format: "esm",
+  plugins: [],
+});
